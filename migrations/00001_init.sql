1f575e1f3c5566b293bc30fa2b8cbd401e4fa524
Sam Lau <esayemm@gmail.com>
2018-08-14 15:04:48 -0700

    [migration] Change email column to be unique

--- migrations/00001_init.sql
+++ migrations/00001_init.sql
@@ -2,7 +2,7 @@
 -- SQL in this section is executed when the migration is applied.
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE "user" (
-  id uuid NOT NULL DEFAULT uuid_generate_v1(),
+  id uuid UNIQUE NOT NULL DEFAULT uuid_generate_v1(),
   email text,
   password text,
   PRIMARY KEY(id)
