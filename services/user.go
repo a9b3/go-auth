963b8f38bafc8362ac50eade61b53bcd93ff6920
Sam Lau <esayemm@gmail.com>
2018-08-13 23:21:25 -0700

    hash and salt user password

--- services/user.go
+++ services/user.go
@@ -3,22 +3,31 @@ package services
 import (
 	"database/sql"
 	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // User is the definition of a user.
 type User struct {
-	Email string
-	ID    string
+	Email    string
+	ID       string
+	Password string
 }
 
 // UserCreate inserts new user row into the user table.
 func UserCreate(db *sql.DB, email string, password string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		panic(fmt.Errorf("fatal error generating password: %s", err))
+	}
+	hashedPassword := string(hash)
+
 	sqlStatement := `
 	INSERT INTO "user" (email, password)
 	VALUES ($1, $2)
 	RETURNING id`
 	var id string
-	err := db.QueryRow(sqlStatement, email, password).Scan(&id)
+	err = db.QueryRow(sqlStatement, email, hashedPassword).Scan(&id)
 	if err != nil {
 		panic(fmt.Errorf("fatal error inserting user: %s", err))
 	}
@@ -29,12 +38,24 @@ func UserCreate(db *sql.DB, email string, password string) string {
 // UserGet returns user given id
 func UserGet(db *sql.DB, id string) User {
 	sqlStatement := `
-	SELECT id, email FROM "user" WHERE id=$1`
+	SELECT id, email, password FROM "user" WHERE id=$1`
 	user := User{}
-	err := db.QueryRow(sqlStatement, id).Scan(&user.ID, &user.Email)
+	err := db.QueryRow(sqlStatement, id).Scan(&user.ID, &user.Email, &user.Password)
 	if err != nil {
 		panic(fmt.Errorf("fatal error querying user: %s", err))
 	}
 
 	return user
+}
+
+// VerifyPassword will return boolean for a password match
+func VerifyPassword(hashedPassword string, password string) bool {
+	byteHash := []byte(hashedPassword)
+	bytePass := []byte(password)
+	err := bcrypt.CompareHashAndPassword(byteHash, bytePass)
+	if err != nil {
+		return false
+	}
+
+	return true
 }
--- services/user_test.go
+++ services/user_test.go
@@ -26,13 +26,21 @@ func TestMain(m *testing.M) {
 }
 
 func TestCreate(t *testing.T) {
-	id := UserCreate(dbInstance, "cool", "asd")
+	email := "cool"
+	password := "asd"
+	id := UserCreate(dbInstance, email, password)
 	user := UserGet(dbInstance, id)
 
-	if user.Email != "cool" {
-		t.Fatalf(`Created user's email "%s" must match original "%s"`, user.Email, "cool")
+	if user.Email != email {
+		t.Fatalf(`Created user's email "%s" must match original "%s"`, user.Email, email)
 	}
 	if user.ID != id {
 		t.Fatalf(`Created user's id "%s" must match original "%s"`, user.ID, id)
+	}
+	if user.Password == password {
+		t.Fatalf(`Created user's password "%s" must not equal original "%s"`, user.Password, password)
+	}
+	if !VerifyPassword(user.Password, password) {
+		t.Fatalf(`Created user's password "%s" must verify with original "%s"`, user.Password, password)
 	}
 }
